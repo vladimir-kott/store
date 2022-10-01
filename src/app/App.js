d984d2616f6371307ec477d61bd43fc771f50ffd
Vladimir Kott <vladimirkott19@gmail.com>
2022-10-01 19:59:14 +0300

    +

--- src/app/App.js
+++ src/app/App.js
@@ -5,12 +5,39 @@ function App() {
     <div className="App">
       <header className="w-full h-full">
         <TextField 
-        label = "тестовий лейбл"
-        type = "text"
-        name = "text_field"
-        value = "Це текстове поле з можливыстю редагування"
-        placeholder = "введіть ваші данні"
-        error="тестова помилка валідації"/>
+          label = "Тестовий лейбл"
+          type = "text"
+          name = "text_field"
+          value = "Це текстове поле з можливыстю редагування"
+          placeholder = "введіть ваші данні"
+          error="тестова помилка валідації"
+        />
+        <TextField 
+          label = "Тестовий лейбл"
+          type = "password"
+          name = "password_field"
+          value = "Це текстове поле з можливыстю редагування"
+          placeholder = "введіть ваші данні"
+          error="тестова помилка валідації"
+        />
+        <TextField 
+          label = "Тестовий лейбл"
+          type = "text"
+          customType="search"
+          name = "search_field"
+          value = "Це текстове поле з можливыстю редагування"
+          placeholder = "введіть ваші данні"
+          error="тестова помилка валідації"
+        />
+        <TextField 
+          label = "Тестовий лейбл"
+          type = "text"
+          customType="any"
+          name = "custom_field"
+          value = "Це текстове поле з можливыстю редагування"
+          placeholder = "введіть ваші данні"
+          error="тестова помилка валідації"
+        />
       </header>
     </div>
   );
--- src/app/components/common/form/textField.jsx
+++ src/app/components/common/form/textField.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const TextField = ({label, type, name, value, onChange, placeholder, error}) => {
-    const defoultClassName = "border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" 
+
+const TextField = ({label, type, customType, name, value, onChange, placeholder, error}) => {
+    const defoultClassNameInput = "bg-gray-50 border border-gray-300 text-gray-900 " +
+     "text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " +
+     "dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white " +
+     "dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+
+     const [showPassword, setShowPassword] = useState(false);
+
+     const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    }
+
+    const handleChange = ({ target }) => {
+        onChange({ name: target.name, value: target.value });
+    };
+
+    console.log('customType', customType)
+
     return ( 
         <div className="">
-            <label htmlFor={name}>{label}</label>
+            <label 
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                htmlFor={name}>{label}
+            </label>
+            <div className="flex">
             <input 
-            type={"text"}
-            id={name}
-            name={name}
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            className={defoultClassName}
+                type={showPassword ? "text" : type}
+                id={name}
+                name={name}
+                value={value}
+                onChange={handleChange}
+                placeholder={placeholder}
+                className={defoultClassNameInput + (customType!=="search" && type==="text" ? "rounded-lg " : "rounded-l-lg ")}
             />
+             {type === "password" && (
+                    <button
+                        className="5 bg-blue-300 rounded-r-lg "
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                    {showPassword ? (<FontAwesomeIcon icon={faEyeSlash}/>) : (<FontAwesomeIcon icon={faEye}/>)}
+                    </button>
+            )}
+            {customType === "search" && (
+                    <button
+                        className="5 bg-blue-300 rounded-r-lg "
+                        type="button"
+                        /*onClick={toggleShowPassword}*/
+                    >
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+            )}
+            </div>
             {error && <div className="">{error}</div>}
         </div>
     );
 }
 TextField.defaultProps = {
-    type: "text"
+    type: "text",
+    customType: "text"
 };
 TextField.propTypes = {
     label: PropTypes.string,
