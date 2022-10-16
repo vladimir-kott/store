eb54784c49bc911324e32b70045c6c2c478c7368
Vladimir Kott <vladimirkott19@gmail.com>
2022-10-16 22:47:17 +0300

    +

--- src/app/App.js
+++ src/app/App.js
@@ -3,13 +3,24 @@ import NavBar from "./components/ui/navBar";
 import Main from "./layouts/main";
 import Carousel from "./components/ui/carousel";
 import Footer from "./components/ui/footer";
+import ProductCard from "./components/ui/card";
 
 function App() {
+
+  const array = [20]
+
   return (
   <div>
     <NavBar/>
     <div className="container mx-auto">
       <Carousel/>
+      <div className="flex">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
       <Main/>
     </div>
     <Footer/>
